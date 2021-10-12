009cea65f9054e010543dba60a82466aa61aaa88
Owenixd <srcrow8@gmail.com>
2021-10-11 20:47:39 -0600

    first commit

---
+++ themes/Personalisado/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Perzonalisado';
+
+$themeFolder = 'Personalisado';
+
+$themeAuthor = 'Fredy';
+
+$themeAuthorUrl = 'mailto:hdplox80@gmail.com';
+
+$themeVirsion = '1.5.2';
+
+$themeImg = $themeFolder . '/temalogo.png';
+?>
---
+++ themes/wowonder/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'FaceChat';
+
+$themeFolder = 'wowonder';
+
+$themeAuthor = 'Deen Doughouz';
+
+$themeAuthorUrl = '';
+
+$themeVirsion = '1.5.6';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
